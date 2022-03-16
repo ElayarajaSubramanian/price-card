8a074bd8169a54fff74bbbf71e9358150c258f81
Elayaraja <raja5ramani@gmail.com>
2022-03-17 02:11:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,176 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Price from "./Price";
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row bg-primary p-4">
+        <div className="col-lg-4">
+          <Price price="0">
+            <ul className="fa-ul">
+              <li>
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Single User
+              </li>
+              <li>
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                5GB Storage
+              </li>
+              <li>
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Unlimited Public Projects
+              </li>
+              <li>
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Community Access
+              </li>
+              <li className="text-muted">
+                <span className="fa-li">
+                  <i className="fas fa-times"></i>
+                </span>
+                Unlimited Private Projects
+              </li>
+              <li className="text-muted">
+                <span className="fa-li">
+                  <i className="fas fa-times"></i>
+                </span>
+                Dedicated Phone Support
+              </li>
+              <li className="text-muted">
+                <span className="fa-li">
+                  <i className="fas fa-times"></i>
+                </span>
+                Free Subdomain
+              </li>
+              <li className="text-muted">
+                <span className="fa-li">
+                  <i className="fas fa-times"></i>
+                </span>
+                Monthly Status Reports
+              </li>
+            </ul>
+          </Price>
+        </div>
+        <div className="col-lg-4">
+          <Price price="9">
+            <ul className="fa-ul">
+              <li>
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                5 Users
+              </li>
+              <li>
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                50GB Storage
+              </li>
+              <li>
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Unlimited Public Projects
+              </li>
+              <li>
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Community Access
+              </li>
+              <li className="text-muted">
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Unlimited Private Projects
+              </li>
+              <li className="text-muted">
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Dedicated Phone Support
+              </li>
+              <li className="text-muted">
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Free Subdomain
+              </li>
+              <li className="text-muted">
+                <span className="fa-li">
+                  <i className="fas fa-times"></i>
+                </span>
+                Monthly Status Reports
+              </li>
+            </ul>
+          </Price>
+        </div>
+        <div className="col-lg-4">
+          <Price price="49">
+            <ul className="fa-ul">
+              <li>
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Unlimited Users
+              </li>
+              <li>
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                150GB Storage
+              </li>
+              <li>
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Unlimited Public Projects
+              </li>
+              <li>
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Community Access
+              </li>
+              <li className="text-muted">
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Unlimited Private Projects
+              </li>
+              <li className="text-muted">
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Dedicated Phone Support
+              </li>
+              <li className="text-muted">
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Free Subdomain
+              </li>
+              <li className="text-muted">
+                <span className="fa-li">
+                  <i className="fas fa-check"></i>
+                </span>
+                Monthly Status Reports
+              </li>
+            </ul>
+          </Price>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Price.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Price.css";
+
+const Price = (props) => {
+  return (
+    <div className="card mb-5 mb-lg-0">
+      <div className="card-body">
+        <h5 className="card-title text-uppercase text-center">Free</h5>
+        <h6 className="card-price text-center">
+          ${props.price}
+          <span className="period">/month</span>
+        </h6>
+        <hr />
+        {props.children}
+        <div className="d-grid">
+          <a
+            href="#"
+            className="btn btn-primary text-uppercase rounded-pill btn-medium py-2"
+          >
+            Button
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Price;
